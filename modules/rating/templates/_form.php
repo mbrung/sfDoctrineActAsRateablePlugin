962e66e37935530298bd4f8922bd48dd2bd14ea4
Michael Brung <michaelbrung@michael-mbp.local>
2010-10-10 20:14:17 +0200

    Added missing echo for rateable model

--- modules/rating/templates/_form.php
+++ modules/rating/templates/_form.php
@@ -1,6 +1,6 @@
 <?php if ($sf_user->isAuthenticated()):?>
 <?php if ($rateable->alreadyRatedBy($sf_user->getGuardUser()->getRawValue())):?>
-<p>You have already rated this <?php $rateable_model?></p>
+<p>You have already rated this <?php echo $rateable_model?></p>
 <?php else:?>
 <form action="<?php echo url_for('@rating_save')?>" method="post">
 <?php echo $form ?>
